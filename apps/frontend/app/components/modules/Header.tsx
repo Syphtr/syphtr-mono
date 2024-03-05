d53c3d39c490e43a20b6c632142e8b5d19181ac0
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-03-05 09:14:33 +0000

    feat: add navbar component - test

--- apps/frontend/app/components/modules/Header.tsx
+++ apps/frontend/app/components/modules/Header.tsx
@@ -3,11 +3,8 @@ import { Link } from "@remix-run/react";
 export default function Header() {
   return (
     <header className="fixed top-0 w-full">
-      <nav
-        className={` bg-transparent hover:bg-white border-gray-200 px-4 lg:px-6 py-2.5  `}
-      >
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          {/* BRAND LOGO */}
+      <nav className="navbar bg-green-500">
+        <div className="flex-1">
           <Link to="/" className="flex items-center">
             <img
               src={"/logos/Black logo - no background.png"}
@@ -15,21 +12,23 @@ export default function Header() {
               alt="Logo"
             />
           </Link>
-          <div>
-            <Link
-              to="/dashboard"
-              className="relative inline-flex items-center justify-center p-3 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group  bg-transparent
-              border-2 border-green-500   focus:ring-4 focus:outline-none focus:ring-lime-200"
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost text-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
             >
-              Login
-            </Link>
-            <Link
-              to="/dashboard"
-              className="relative inline-flex items-center justify-center p-3 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
-            >
-              Dashboard
-            </Link>
-          </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>
+            </svg>
+          </button>
         </div>
       </nav>
     </header>
