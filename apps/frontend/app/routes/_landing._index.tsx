3232331a9d5a14b51d452c93a780a78999a855f9
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-03-06 00:43:07 +0000

    build install prisma + initial setup

--- apps/frontend/app/routes/_landing._index.tsx
+++ apps/frontend/app/routes/_landing._index.tsx
@@ -1,10 +1,14 @@
-import type { MetaFunction } from "@remix-run/node";
+import { type MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { prisma } from "~/db.server";
 
 // components
 import Hero from "~/components/modules/Hero";
 import Features from "~/components/modules/Features";
 import Pricing from "~/components/modules/Pricing";
 import SubscriptionBanner from "~/components/modules/SubscriptionBanner";
+// import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,7 +17,18 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const users = await prisma.profile.findMany();
+  return json({
+    users,
+  });
+};
+
 export default function Index() {
+  const { users } = useLoaderData<typeof loader>();
+
+  console.log(users);
+
   return (
     <>
       <Hero />
